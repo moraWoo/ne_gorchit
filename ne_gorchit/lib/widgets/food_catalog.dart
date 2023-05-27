dc580840f658561b67f86e57ac7bcd7447b124cf
moraWoo <pkstratech@yandex.ru>
2023-05-28 01:24:45 +0600

    added bottomTabBar

--- ne_gorchit/lib/widgets/food_catalog.dart
+++ ne_gorchit/lib/widgets/food_catalog.dart
@@ -5,10 +5,17 @@ import 'package:http/http.dart' as http;
 import 'package:ne_gorchit/services/network_manager.dart';
 import 'package:ne_gorchit/widgets/basket.dart';
 
-class FoodMenu extends StatelessWidget {
-  const FoodMenu({
+class FoodMenu extends StatefulWidget {
+  FoodMenu({
     super.key,
   });
+
+  @override
+  State<FoodMenu> createState() => _FoodMenuState();
+}
+
+class _FoodMenuState extends State<FoodMenu> {
+  int tabIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -52,6 +59,77 @@ class FoodMenu extends StatelessWidget {
           }
         },
       ),
+      bottomNavigationBar: Visibility(
+        visible: true,
+        child: bottomWidget(),
+      ),
+    );
+  }
+}
+
+class bottomWidget extends StatelessWidget {
+  const bottomWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 120,
+      color: Colors.white,
+      child: InkWell(
+        onTap: () => print('tap on close'),
+        child: Container(
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.only(
+                topRight: Radius.circular(20), topLeft: Radius.circular(20)),
+            boxShadow: [
+              BoxShadow(
+                  color: Color.fromRGBO(48, 47, 45, 1),
+                  spreadRadius: 0,
+                  blurRadius: 0.1),
+            ],
+          ),
+          child: ClipRRect(
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(20.0),
+              topRight: Radius.circular(20.0),
+            ),
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+              child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll(Colors.yellow),
+                    minimumSize: MaterialStatePropertyAll(
+                      Size(80, 50),
+                    ),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                    )),
+                  ),
+                  onPressed: () {},
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        '25-35 мин',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      Text(
+                        'Заказ',
+                        style: TextStyle(color: Colors.black, fontSize: 20),
+                      ),
+                      Text(
+                        '3005 ₸',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ],
+                  )),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
@@ -72,9 +150,10 @@ class FoodItem extends StatefulWidget {
 
 class _FoodItemState extends State<FoodItem> {
   var imgUrl = 'http://localhost:4000/';
-
   bool _isButtonDisabled = false;
+  bool onClicked = false;
   int counter = 1;
+
   void hideButton() {
     setState(() {
       _isButtonDisabled = !_isButtonDisabled;
@@ -82,7 +161,26 @@ class _FoodItemState extends State<FoodItem> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    onClicked = false;
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    bool bottomVisible = true;
+    bool secondOnClicked;
+    if (onClicked) {
+      bottomVisible = true;
+    } else {
+      bottomVisible = false;
+    }
+
     return ListView.builder(
       itemCount:
           widget.items.fold<int>(0, (count, menu) => count + menu.data.length),
@@ -182,13 +280,13 @@ class _FoodItemState extends State<FoodItem> {
                               padding: const EdgeInsets.all(15.0),
                               child: ElevatedButton(
                                 onPressed: () {
-                                  if (widget.counter != 0) {
+                                  if (widget.counter == 1) {
+                                    hideButton();
+                                  } else {
                                     setState(() {
-                                      widget.counter == 0
-                                          ? print('counter at 1')
-                                          : widget.counter--;
+                                      widget.counter--;
                                     });
-                                  } else {}
+                                  }
                                 },
                                 style: ElevatedButton.styleFrom(
                                     shape: RoundedRectangleBorder(
